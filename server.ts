840ef478d5fd913d0220656bffce8b2743a15696
Anish Satalkar <anishsatalkar@gmail.com>
2022-10-26 15:41:02 -0400

    Fixed connection string.

--- server.ts
+++ server.ts
@@ -28,9 +28,10 @@ const PROTOCOL = "mongodb+srv";
 const DB_USERNAME = process.env.DB_USERNAME;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const HOST = "cluster0.aoajxux.mongodb.net";
-const DB_NAME = "myFirstDatabase";
+// const DB_NAME = "myFirstDatabase";
 const DB_QUERY = "retryWrites=true&w=majority";
-const connectionString = `${PROTOCOL}://${DB_USERNAME}:${DB_PASSWORD}@${HOST}/${DB_NAME}?${DB_QUERY}`;
+const connectionString = `${PROTOCOL}://${DB_USERNAME}:${DB_PASSWORD}@${HOST}/?${DB_QUERY}`;
+// const connectionString = `${PROTOCOL}://${DB_USERNAME}:${DB_PASSWORD}@${HOST}/${DB_NAME}?${DB_QUERY}`;
 // connect to the database
 mongoose.connect(connectionString);
 
