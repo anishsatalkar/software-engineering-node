6eedbba2ae964bb783e98b200635320ef3f0adc6
Anish Satalkar <anishsatalkar@gmail.com>
2022-11-19 12:42:47 -0500

    Added all auth controllers.

--- controllers/AuthController.ts
+++ controllers/AuthController.ts
@@ -29,7 +29,50 @@ const AuthenticationController = (app: Express) => {
             res.json(insertedUser);
         }
     }
+
+    const profile = (req: any, res: any) => {
+        const profile = req.session['profile'];
+        if (profile) {
+            profile.password = "";
+            res.json(profile);
+        } else {
+            res.sendStatus(403);
+        }
+    }
+
+    const logout = (req: any, res: any) => {
+        req.session.destroy();
+        res.sendStatus(200);
+    }
+
+    const login = async (req: any, res: any) => {
+        const user = req.body;
+        const username = user.username;
+        const password = user.password;
+        const existingUser = await userDao
+            .findUserByUsername(username);
+
+        if (!existingUser) {
+            res.sendStatus(403);
+            return;
+        }
+
+        const match = await bcrypt
+            .compare(password, existingUser.password);
+
+        if (match) {
+            existingUser.password = '*****';
+            req.session['profile'] = existingUser;
+            res.json(existingUser);
+        } else {
+            res.sendStatus(403);
+        }
+    };
+
     app.post("/api/auth/signup", signup);
+    app.post("/api/auth/profile", profile);
+    app.post("/api/auth/logout", logout);
+    app.post("/api/auth/login", login);
 }
 
 export default AuthenticationController;
